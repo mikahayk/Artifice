a7793fa6b501574f21747ce896735ec49e88ac8f
nicktanic <nicktanic@gmail.com>
2019-11-14 21:58:51 -0500

    added additional debug log

--- Assets/Scripts/NetworkedPlayer.cs
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -25,6 +25,36 @@ public class NetworkedPlayer : NetworkBehaviour
         }
     }
 
+    // when connection established
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log("on start server");
+        Debug.Log("isserver" + isServer);
+
+        if (isServer)
+        {
+            Debug.Log("net id" + netId);
+        }
+        Debug.Log("On start server end");
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        Debug.Log("On start Player");
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        Debug.Log("isserver" + isServer);  // is this server machine
+        Debug.Log("isserver only " + isServerOnly); // is this machine act as server only
+        Debug.Log("is client" + isClient);
+        Debug.Log("is client only" + isClientOnly);
+    }
+
+
 
     // Local function to update my player type
     public void updateLocalPlayerType(int type)
