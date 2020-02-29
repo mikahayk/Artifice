9e25fa86e2f7df98cb2b1979bbfcd9cc68dc3a1a
mikahayk <mikahayk@gmail.com>
2020-02-29 00:15:30 -0500

    ver 2.0

---
+++ Assets/Scripts/Onboarding/player_IK.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_IK : MonoBehaviour
+{
+
+    public GameObject leftIK;
+    public GameObject rightIK;
+    public GameObject headIK;
+
+    public GameObject leftController;
+    public GameObject rightController;
+
+    public GameObject playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	void Update()
+	{
+	    leftIK.transform.position = leftController.transform.position;
+	    rightIK.transform.position = rightController.transform.position;
+	    headIK.transform.position = playerCamera.transform.position;
+
+	}
+}
---
+++ Assets/Scripts/Onboarding/switchCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCamera : MonoBehaviour
+{
+
+    public GameObject roomCamera;
+    public GameObject playerCamera;
+    public GameObject player;
+
+    void Switch()
+    {
+        roomCamera.SetActive(false);
+        playerCamera.SetActive(true);
+        player.SetActive(true);
+}
+}
